46c485da68aaea71982b15e75ae32efdd2891662
EC2 Default User <ec2-user@ip-172-31-3-179.ec2.internal>
2020-11-17 13:31:21 +0000

    RVS

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,22 +1,37 @@
 class Public::AddressesController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def new
+  end
+
+  def index
+    @customers = Customers.all
+  end
+
   def edit
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
 
-  def index
-  end
+  # private
 
-  def create
-  end
+  # def customer_params
+  #   params.require(:user).permit(:name, :introduction, :profile_image)
+  # end
 
-  def show
-  end
+  # def ensure_correct_user
+  #   @user = User.find(params[:id])
+  #   unless @user == current_user
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
 
-  def destroy
-  end
-
-  def new
-  end
 end
