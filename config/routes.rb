620c15417579f58a1771873437f831a27daf9da1
EC2 Default User <ec2-user@ip-172-31-3-179.ec2.internal>
2020-11-19 07:13:05 +0000

    add admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+    get 'items/new'
+    get 'items/create'
+  end
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
     get 'genres/index'
     get 'genres/create'
     get 'genres/edit'
