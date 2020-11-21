999a2dcc482781d4d1d883e2686d0770cc04bcf3
EC2 Default User <ec2-user@ip-172-31-3-179.ec2.internal>
2020-11-21 15:32:55 +0000

    add_admin_customer

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ApplicationRecord
 
   has_many :items
+  # def self.admin_id
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,11 @@
 Rails.application.routes.draw do
 
-
-  namespace :admin do
-    # get 'items/index'
-    # # get 'items/show'
-    # get 'items/:id' => 'items#show'
-    # get 'items/edit'
-    # get 'items/update'
-    # get 'items/new'
-    # post 'items/create'
-    resources :items, except:[:destroy]
-  end
-
-
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-    patch 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    patch 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    post 'genres/create'
-    get 'genres/edit'
-    patch 'genres/update'
-  end
-
-  
  #管理者
 
-  get '/admins', to: 'admins/homes#top'
-  devise_for :admins
+  get '/admin', to: 'admin/homes#top'
+  devise_for :admin
 
-  namespace :admins do
+  namespace :admin do
     resources :customers, only:[:index, :show, :edit, :update]
     resources :orders, only:[:index, :show, :update]
     resources :order_details, only:[:update]
@@ -52,13 +17,17 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
 
-
+  resources :customers, only:[:edit, :update]
   devise_for :customers
 
-  resources :customers, only:[:edit, :update]
-  get '/customers/my_page' => 'customers#my_page'
-  get '/customers/unsubscribe' => 'customers#unsubscribe'
-  patch '/end_users/withdraw' => 'end_users#withdraw'
+  # scope module: :public do
+  #   collection do
+  #     resources :customers, only:[:edit, :update] do
+  #       get :my_page
+  #       get :unsubscribe
+  #     end
+  #   end
+  # end
 
   resources :cart_items, only:[:index, :update, :destroy, :create]
   delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
@@ -69,13 +38,5 @@ Rails.application.routes.draw do
   get '/orders/complete' => 'orders#complete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :public do
-  resources :addresses, :homes, :items, :cart_items, :orders
-  end
-
-  namespace :admins do
-  resources :homes, :items, :orders , :genres, :order_details
-  end
 
 end
