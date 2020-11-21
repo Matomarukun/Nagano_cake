97c9084828747a18896d01cc11cf9ee6a19639e5
yuki-ksy <yuki.k.97sy@gmail.com>
2020-11-21 07:55:46 +0000

    cart途中

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_055925) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
