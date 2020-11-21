f8f3473eeaa8829bc32f619bc4051c66d6b47381
EC2 Default User <ec2-user@ip-172-31-20-175.ec2.internal>
2020-11-21 10:12:27 +0000

    my_page追加.routes.rbも変わってます

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,19 +1,6 @@
 class Public::CustomersController < ApplicationController
   def my_page
-     @customer = Customer.find(params[:id])
+     @customer = current_customer
   end
-
-  def edit
-       @customer = Customer.find(params[:id])
-    if @customer == current_customer
-      render "edit"
-    else
-      redirect_to  customers_my_page_path(current_customer)
-    end
-  end
-
-  def update
-  end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,26 @@
 Rails.application.routes.draw do
-
-  
  #管理者
-  get '/admins', to: 'admins/homes#top'
-  devise_for :admins
-
-  namespace :admins do
+  get '/admin', to: 'admin/homes#top'
+  devise_for :admin
+  namespace :admin do
     resources :customers, only:[:index, :show, :edit, :update]
     resources :orders, only:[:index, :show, :update]
     resources :order_details, only:[:update]
     resources :items, except:[:destroy]
     resources :genres, only:[:index, :edit, :create, :update]
   end
-
-
+  #会員
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
-
-
   devise_for :customers
-
-  resources :customers, only:[:edit, :update]
-  get '/customers/my_page' => 'customers#my_page'
-  get '/customers/unsubscribe' => 'customers#unsubscribe'
-  patch '/end_users/withdraw' => 'end_users#withdraw'
-
+  scope module: :public do
+      resources :customers, only:[:edit, :update] do
+      collection do
+        get :my_page
+        get :unsubscribe
+      end
+    end
+  end
   resources :cart_items, only:[:index, :update, :destroy, :create]
   delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :addresses, only:[:index, :create, :edit, :update, :destroy]
@@ -32,15 +28,5 @@ Rails.application.routes.draw do
   resources :orders, only:[:index, :create, :show, :new]
   post '/orders/confirm' => 'orders#confirm'
   get '/orders/complete' => 'orders#complete'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :public do
-   resources :addresses, :homes, :items, :cart_items, :orders
-  end
-
-  namespace :admins do
-   resources :homes, :items, :orders , :genres, :order_details
-  end
-
 end
