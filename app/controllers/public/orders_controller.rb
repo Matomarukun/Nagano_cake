2b4e57aaf65942ba5486d8e18a8d283dddedf4f3
EC2 Default User <ec2-user@ip-172-31-3-179.ec2.internal>
2020-11-25 12:17:22 +0000

    order_new,logo

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,19 +14,6 @@ class Public::OrdersController < ApplicationController
   def index
   end
 
-
-  def create
-    @address = Address.new(address_params)
-    @address.customer_id = current_customer.id
-
-    if @address.save
-      redirect_to addresses_path
-    else
-      @addresses = Address.all
-      render 'index'
-    end
-  end
-
   private
     def order_params
       params.require(:order).permit(:customer_id, :status, :order_sum, :method_pay, :name, :postal_code, :address)
